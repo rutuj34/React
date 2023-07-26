acf35e382576e57e74caac79b524cda5ccf0a81c
rutuj34 <rutuj.ghodekar34@gmail.com>
2023-07-26 21:53:10 +0530

    initial

---
+++ app.js
@@ -0,0 +1,7 @@
+const Heading1 = React.createElement("h1",{className:"heading1"},"This is Heading 1");
+
+const Heading2 = React.createElement("h1",{className:"heading2"},"This is Heading 2");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render([Heading1,Heading1]);
